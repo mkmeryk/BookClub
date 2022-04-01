f874ef52b787a952f2863a56a2d9a233631ee1f9
khoa nguyen-gray <khoanguyengray@gmail.com>
2022-04-01 07:48:43 -0600

    add seeds, fix books path error and seed db

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :book
-  belongs_to :user
+  belongs_to :user  
   has_many :likes, dependent: :destroy
-  has_many :likers,  dependent: :destroy, source: :user
+  has_many :likers,  through: :likes, source: :user
 
   validates :body, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Like.destroy_all
+Review.destroy_all
+Book.destroy_all
+User.destroy_all
+
+PASSWORD = "123"
+
+approvals = ['nil', 'approved', 'rejected', 'pending']
+
+5.times do
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+    User.create(
+        first_name: first_name,
+        last_name: last_name,
+        email: "#{first_name}@#{last_name}.com",
+        password: PASSWORD
+    )
+end
+
+users = User.all
+
+20.times do
+    created_at = Faker::Date.backward(days:365 * 5)
+    b = Book.create(
+        title: Faker::Book.title,
+        genre: Faker::Book.genre,  
+        image_url: Faker::LoremFlickr.image,      
+        introduction: Faker::Movie.quote,
+        user: users.sample
+    )
+    if b.valid?
+        rand(1..3).times do
+            r = Review.create(
+                body: Faker::Quotes::Shakespeare.hamlet_quote,
+                approval: approvals.sample,
+                book: b,
+                user: users.sample,
+                likers: users.shuffle.slice(0, rand(users.count))
+            )
+        end
+    end
+end
+
+books = Book.all
+revies = Review.all
+likes = Like.all
+
+puts Cowsay.say("Generated #{books.count} books", :frogs)
+puts Cowsay.say("Generated #{reviews.count} reviews", :cow)
+puts Cowsay.say("Generated #{users.count} users", :koala)
+puts Cowsay.say("Generated #{likes.count} likes", :dragon)
